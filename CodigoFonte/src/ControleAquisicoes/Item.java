a791c197d2a69e0a49ef73caaadf33534d99ad67
Eduardo Faé <eduardofaeef@gmail.com>
2021-10-07 15:09:42 -0300

    Add Atributo Quantidade em Item

--- CodigoFonte/src/ControleAquisicoes/Item.java
+++ CodigoFonte/src/ControleAquisicoes/Item.java
@@ -4,11 +4,13 @@ public class Item
     private int idItem;
     private String descricao;
     private double valor;
+    private int quantidade;
 
-    public Item(int idItem, String descricao, double valor)
+    public Item(int idItem, String descricao, double valor, int quantidade)
     {
         this.idItem = idItem;
         this.descricao = descricao;
         this.valor = valor;
+        this.quantidade = quantidade;
     }
 }
