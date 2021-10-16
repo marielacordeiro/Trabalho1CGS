c50b753e7c516b247b15b02c888ba2b24a8fd984
Pedro Semensato <semensatopedro@gmail.com>
2021-10-16 14:48:28 -0300

    Adição de Funcionários + Getters

--- CodigoFonte/src/ControleAquisicoes/Departamento.java
+++ CodigoFonte/src/ControleAquisicoes/Departamento.java
@@ -1,12 +1,37 @@
 package ControleAquisicoes;
-public class Departamento
-{
-    public String nome;
-    public double valorMaximoPedido;
 
-    //Commit inicial
+import java.util.ArrayList;
+
+public class Departamento
+
+{
+    private String nome;
+    private double valorMaximoPedido;
+    private ArrayList<Funcionario> funcionarios;
+
+    
     public Departamento(String nome, double valorMaximoPedido) {
         this.nome = nome;
         this.valorMaximoPedido = valorMaximoPedido;
+        this.funcionarios = new ArrayList<Funcionario>();
     }
+
+    public void addFuncionario(Funcionario funcionario) throws IllegalArgumentException{
+
+        //Valida se não estamos adicionando o mesmo funcionário novamente
+        if(funcionarios.contains(funcionario)){
+            throw new IllegalArgumentException();
+        }
+
+        this.funcionarios.add(funcionario);
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public double getValorMaximoPedido(){
+        return this.valorMaximoPedido;
+    }
+
 }
