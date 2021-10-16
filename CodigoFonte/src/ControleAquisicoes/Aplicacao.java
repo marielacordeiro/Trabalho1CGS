b78c041f506082c2f81ca6d038e1f9eeb9932d0f
lucasdellatorre <lucasdellatorre222@gmail.com>
2021-10-16 16:00:43 -0300

    Inicializando os departamentos

--- CodigoFonte/src/ControleAquisicoes/Aplicacao.java
+++ CodigoFonte/src/ControleAquisicoes/Aplicacao.java
@@ -4,6 +4,11 @@ public class Aplicacao
     public void inicializa()
     {
         //inicializa os funcionarios no departamento
+        Departamento d1 = new Departamento("Financeiro", 1000000);
+        Departamento d2 = new Departamento("RH", 100000);
+        Departamento d3 = new Departamento("Engenharia", 500000);
+        Departamento d4 = new Departamento("Administracao", 2000000);
+        Departamento d5 = new Departamento("Marketing", 600000);
     }
 
     public void menu()
