de98426368fecb4e274ce41448255ab30c5342b1
marielacordeiro <marielapontes@icloud.com>
2021-10-19 20:11:22 -0300

    Fix: nome de objeto invalido

--- CodigoFonte/src/ControleAquisicoes/Historico.java
+++ CodigoFonte/src/ControleAquisicoes/Historico.java
@@ -1,11 +1,11 @@
 package ControleAquisicoes;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Historico
-{
+public class Historico {
     private ArrayList<Pedido> listaPedidos;
 
     public Historico() {
@@ -17,35 +17,38 @@ public class Historico
             listaPedidos.add(pedido);
     }
 
-    public List<Pedido> pedidosEntreDuasDatas(LocalDate dataInicial, LocalDate dataFinal){
-        List<Pedido> pedidosEntreDatas = listaPedidos.stream().filter(p -> p.getDataPedido().isAfter(dataInicial) && p.getDataPedido().isBefore(dataFinal)).collect(Collectors.toList());
+    public List<Pedido> pedidosEntreDuasDatas(LocalDate dataInicial, LocalDate dataFinal) {
+        List<Pedido> pedidosEntreDatas = listaPedidos.stream()
+                .filter(p -> p.getDataPedido().isAfter(dataInicial) && p.getDataPedido().isBefore(dataFinal))
+                .collect(Collectors.toList());
         return pedidosEntreDatas;
     }
 
-    public List<Pedido> buscarPedidosFuncionario(Funcionario funcionario) {
-        List<Pedido> pedidosFuncionario = listaPedidos.stream().filter(p -> p.getFuncSolicitante() == funcionario).collect(Collectors.toList());
+    public List<Pedido> buscarPedidosFuncionario(Usuario funcionario) {
+        List<Pedido> pedidosFuncionario = listaPedidos.stream().filter(p -> p.getFuncSolicitante() == funcionario)
+                .collect(Collectors.toList());
         return pedidosFuncionario;
     }
 
     public List<Pedido> buscarPedidosDescricaoItem(String descricao) {
-        List<Pedido> pedidosDescricaoItem = listaPedidos.stream().filter(p -> p.getItens()
-                                                        .stream().filter(i -> i.getDescricao()
-                                                        .equals(descricao)).count() > 0).collect(Collectors.toList());
+        List<Pedido> pedidosDescricaoItem = listaPedidos.stream()
+                .filter(p -> p.getItens().stream().filter(i -> i.getDescricao().equals(descricao)).count() > 0)
+                .collect(Collectors.toList());
         return pedidosDescricaoItem;
     }
 
-    public String visualizarPedido(int identificador){
+    public String visualizarPedido(int identificador) {
         if (listaPedidos.size() == 0)
             return "Nenhum pedido registrado neste histórico.";
 
         Pedido pedidoBuscado = null;
-        for (Pedido p: listaPedidos) {
-            if(p.getIdPedido() == identificador){
+        for (Pedido p : listaPedidos) {
+            if (p.getIdPedido() == identificador) {
                 pedidoBuscado = p;
             }
         }
 
-        if(pedidoBuscado == null){
+        if (pedidoBuscado == null) {
             return "Nenhum pedido com esse id";
         }
 
@@ -57,8 +60,8 @@ public class Historico
             return "Nenhum pedido registrado neste histórico.";
 
         int totalAbertos = 0, totalAprovados = 0, totalReprovados = 0;
-        
-        for (Pedido p: listaPedidos) {
+
+        for (Pedido p : listaPedidos) {
             if (p.getStatus().equals("APROVADO"))
                 totalAprovados++;
             else if (p.getStatus().equals("REPROVADO"))
@@ -71,39 +74,42 @@ public class Historico
         int percentualAprovados = totalAprovados * 100 / listaPedidos.size();
         int percentualReprovados = totalReprovados * 100 / listaPedidos.size();
 
-        return "Total de pedidos: " + listaPedidos.size() + 
-               " || Abertos: " + totalAbertos + " (" + percentualAbertos + 
-               "%) || Aprovados: " + totalAprovados + " (" + percentualAprovados +
-               "%) || Reprovados: " + totalReprovados + " (" + percentualReprovados + "%)";
+        return "Total de pedidos: " + listaPedidos.size() + " || Abertos: " + totalAbertos + " (" + percentualAbertos
+                + "%) || Aprovados: " + totalAprovados + " (" + percentualAprovados + "%) || Reprovados: "
+                + totalReprovados + " (" + percentualReprovados + "%)";
     }
 
-    public List<Pedido> pedidosUltimos30Dias(){
+    public List<Pedido> pedidosUltimos30Dias() {
         LocalDate data30DiasAtras = LocalDate.now().minusDays(30);
-        List<Pedido> pedidosDe30DiasAtras =  listaPedidos.stream().filter(p -> p.getDataPedido().isAfter(data30DiasAtras)).collect(Collectors.toList());
+        List<Pedido> pedidosDe30DiasAtras = listaPedidos.stream()
+                .filter(p -> p.getDataPedido().isAfter(data30DiasAtras)).collect(Collectors.toList());
         return pedidosDe30DiasAtras;
     }
 
-    public String valorTotalCategoria(){
+    public String valorTotalCategoria() {
         List<Pedido> pedidosUltimos30Dias = this.pedidosUltimos30Dias();
-        double totalEmAberto = pedidosUltimos30Dias.stream().filter(p -> p.getStatus().equals("ABERTO")).mapToDouble(p -> p.getValorTotal()).sum();
-        double totalAprovado = pedidosUltimos30Dias.stream().filter(p -> p.getStatus().equals("APROVADO")).mapToDouble(p -> p.getValorTotal()).sum();
-        double totalReprovado = pedidosUltimos30Dias.stream().filter(p -> p.getStatus().equals("REPROVADO")).mapToDouble(p -> p.getValorTotal()).sum();
-        return "Total em aberto: R$ " + String.format("%.2f", totalEmAberto) +
-               "\nTotal aprovado: R$ " + String.format("%.2f", totalAprovado) +   
-               "\nTotal reprovado: R$ " + String.format("%.2f", totalReprovado);
+        double totalEmAberto = pedidosUltimos30Dias.stream().filter(p -> p.getStatus().equals("ABERTO"))
+                .mapToDouble(p -> p.getValorTotal()).sum();
+        double totalAprovado = pedidosUltimos30Dias.stream().filter(p -> p.getStatus().equals("APROVADO"))
+                .mapToDouble(p -> p.getValorTotal()).sum();
+        double totalReprovado = pedidosUltimos30Dias.stream().filter(p -> p.getStatus().equals("REPROVADO"))
+                .mapToDouble(p -> p.getValorTotal()).sum();
+        return "Total em aberto: R$ " + String.format("%.2f", totalEmAberto) + "\nTotal aprovado: R$ "
+                + String.format("%.2f", totalAprovado) + "\nTotal reprovado: R$ "
+                + String.format("%.2f", totalReprovado);
     }
 
-    public String detalhePedidoMaiorAquisicao(){
+    public String detalhePedidoMaiorAquisicao() {
         double pedidoMaiorValor = 0;
         Pedido pedido = null;
 
-        for (Pedido p: listaPedidos) {
-           if (!p.getStatus().equals("APROVADO") && !p.getStatus().equals("REPROVADO")) {
+        for (Pedido p : listaPedidos) {
+            if (!p.getStatus().equals("APROVADO") && !p.getStatus().equals("REPROVADO")) {
                 if (p.getValorTotal() > pedidoMaiorValor) {
                     pedidoMaiorValor = p.getValorTotal();
                     pedido = p;
                 }
-           }
+            }
         }
 
         if (pedido == null)
